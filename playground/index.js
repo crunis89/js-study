33d5ee72e7fba5ac8a49c0de5a4d5a2f72e36d53
crunis89 <crunis89@gmail.com>
2020-02-21 17:54:54 +0900

    add documents

--- playground/index.js
+++ playground/index.js
@@ -1,8 +1,18 @@
-var x = 1;
-var y = 2;
+// var x = 1;
+// var y = 2;
 
-// do something
-var z = 0;
-z = x;
+// // do something
+// var z = 0;
+// z = x;
 
-console.log(x, y); // 2 1
+// console.log(x, y); // 2 1
+
+
+// var first = '1';
+// var last = 'Lee';
+
+// // ES5: 문자열 연결
+// console.log('My name is ' + first + ' ' + last + '.');
+
+var key = Symbol('key');
+console.log(typeof key); // symbol
