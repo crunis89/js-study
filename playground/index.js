62cdf062e61df3dae89ad73aa2e11cb8f95188f2
crunis89 <crunis89@gmail.com>
2020-02-25 17:51:56 +0900

    documents upload

--- playground/index.js
+++ playground/index.js
@@ -14,5 +14,47 @@
 // // ES5: 문자열 연결
 // console.log('My name is ' + first + ' ' + last + '.');
 
-var key = Symbol('key');
-console.log(typeof key); // symbol
+// var key = Symbol('key');
+// console.log(typeof key); // symbol
+
+// var i = 5;
+// var numKind;
+
+// if (i < 5){
+//   numKind = '작음'
+// }
+// else if (i > 5){
+//   numKind = '큼'
+// }
+// else (i == 5);{
+//   numKind = '같음'
+// };
+
+// console.log(numKind);
+
+// var x = 5;
+// console.log (x);
+// x++;
+// console.log(x);
+
+// var y = true;
+// +true;
+// console.log(y);
+// var a = 'Hello';
+// ('Hello').toUpperCase;
+// console.log(a);
+
+// const o = {};
+// o.a = 3;
+// console.log(o);
+
+
+// 참조에 의한 전달
+var person = {
+  name: 'Lee'
+};
+
+// 참조 값을 복사
+var copy = person;
+
+console.log(copy);
