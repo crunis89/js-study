ff75d1574aca442781cb6f6e6bc994bda69c92d7
crunis89 <crunis89@gmail.com>
2020-02-27 01:44:34 +0900

    스코프 내용추가

--- playground/function.js
+++ playground/function.js
@@ -18,11 +18,30 @@ console.log(result); // 7
 
 // doSomething();
 
+// var functionName = function(arg0, arg1, arg2){
 
+//   alert("hi");
+// }
+// functionName();
 
+var x = 'global';
 
-var functionName = function(arg0, arg1, arg2){
-
-  alert("hi");
+function foo() {
+  var x = 'local';
+  console.log(x); // ①
 }
-functionName();
+
+foo();
+
+console.log(x); // ②
+
+// var를 스코프내에 중복으로 사용했을 때
+function foo() {
+  var x = 1;
+  // var 키워드로 선언된 변수는 같은 스코프 내에서 중복 선언을 허용한다.
+  // 아래 변수 선언문은 자바스크립트 엔진에 의해 var 키워드가 없는 것처럼 동작한다.
+  var x = 2;
+  console.log(x); // 2
+}
+foo();
+
