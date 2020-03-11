bcc8751078bc36f155da06b56abc19e4c0b8146b
crunis89 <crunis89@gmail.com>
2020-03-11 21:53:23 +0900

    업로드

--- playground/function.js
+++ playground/function.js
@@ -173,17 +173,115 @@
 // // 프로퍼티 어트리뷰트 정보를 제공하는 프로퍼티 디스크립터 객체를 취득한다.
 // console.log(Object.getOwnPropertyDescriptor(person, 'nameT'));
 
-const person = {
-  // 데이터 프로퍼티
-  firstName: 'Ungmo',
-  lastName: 'Lee',
+// const person = {
+//   // 데이터 프로퍼티
+//   firstName: 'Ungmo',
+//   lastName: 'Lee',
 
-  get fullName() {
-	return `${this.firstName} ${this.lastName}`;
-  },
-  set fullName(name) {
-	[this.firstName, this.lastName] = name.split(' ')	; //['foo', 'bar']
-  },
-};
-person.fullName = 'foo bar';
-console.log(person);
+//   get fullName() {
+// 	return `${this.firstName} ${this.lastName}`;
+//   },
+//   set fullName(name) {
+// 	[this.firstName, this.lastName] = name.split(' ')	; //['foo', 'bar']
+//   },
+// };
+// person.fullName = 'foo bar';
+// console.log(person);
+
+
+// const arg = {
+//   0:1,
+//   0:2,
+//   0:3,
+//   length:3;
+// }
+
+// for (let i = 0; i < arg.length; i++){
+//   console.log(arg);
+// }
+
+// function sum() {
+//   // console.log(arguments);
+  
+//   let sum = 0;
+//   for(let i = 0; i < arguments.length; i++) {
+//   // console.log();
+//   sum += arguments[i];
+//   }
+
+//   return sum;
+// }
+// console.log(sum(1,2,3,4,5));
+
+// function linearSearch(array, target) {
+ 
+//   for(let i = 0; i < array.length; i ++){
+//     if(array[i] === target) return i;
+//   }
+//   return -1;
+// }
+
+// function linearSearch(array, target) {
+//   let a = '';
+//   for(let i = 0; i < array.length; i ++){
+//     if(array[i] === target) a = i;
+//     else if(a === '') a = -1;
+//   }
+//   return a;
+// }
+
+// // function linearSearch(array, target) {
+// //   let a = '';
+// //   for(let i = -1; i < array.length; i ++){
+// //     if(array[i] === target) return a = i;
+// //     else if(a === '') a = -1;
+// //   }
+// // }
+
+// console.log(linearSearch([1, 2, 3, 4, 5, 6], 1)); // 0
+// console.log(linearSearch([1, 2, 3, 4, 5, 6], 3)); // 2
+// console.log(linearSearch([1, 2, 3, 4, 5, 6], 5)); // 4
+// console.log(linearSearch([1, 2, 3, 4, 5, 6], 6)); // 5
+// console.log(linearSearch([1, 2, 3, 4, 5, 6], -1)); // -1
+
+// function Circle(radius) {
+//   // 생성자 함수 내부의 this는 생성자 함수가 생성할 인스턴스를 가리킨다.
+//   this.radius = radius;
+//   this.getDiameter = function () {
+//     return 2 * this.radius;
+//   };
+// }
+
+// const circle3 = Circle(15);
+// console.log(circle3); // undefined
+
+// 일반 함수로서 호출된 Circle내의 this는 전역 객체를 가리킨다.
+
+
+
+// *위에서부터 무조건 순차적으로 실행되는 것이 아닌, 일단 선언만하고ㅡ 실행은 호출할때 실행된다 
+function Hello(row) {
+  console.log(this); // new연산자를 통한 생성자 함수를 사용하면 빈객체가 먼저 함수최상단에서 만들고 this에 바인딩함
+
+  this.row2 = row; // 객체에 동적으로 프로퍼티 키 와 값을 추가
+
+  console.log(this); // 잘들어갔나 확인
+
+  this.getDiameter = function () { // this에 들어가있는 {row2 :5}객체에 메소드 추가/프로퍼티에 함수가 들어가서 메소드임
+    return 10 * this.row2; //마지막 메소드를 실행할때 this에 {row2:5, getDiameter : function //아직까지는 안에 뭐가있는지 모르기 떄문에}
+  };
+
+  console.log(this);
+
+}
+const hello1 = new Hello(5); //호출 순서2번
+console.log(hello1.getDiameter()); // 함수값을 실행했을때 this가 함수최상단에서 자신을 호출한 객체를 바인딩하고,
+                                  // 함수값을 계산하고 그값이 나옴
+
+// let c = 3;
+
+const x = {};
+
+x.a = 1;
+
+console.log(x.a);
