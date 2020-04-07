0912858154bad6c6847cd0a423256dacc040e35a
crunis89 <crunis89@gmail.com>
2020-04-07 17:50:41 +0900

    복습

---
+++ playground/test1.js
@@ -0,0 +1,11 @@
+
+
+function foo(param, ...rest) {
+  console.log(param); // 1
+  console.log(rest);  // [ 2, 3 ]
+}
+
+// 스프레드 문법은 배열과 같은 이터러블을 펼쳐서 개별적인 값들의 목록을 만든다.
+// [1, 2, 3] -> 1, 2, 3
+foo(...[1, 2, 3]);
+foo();
